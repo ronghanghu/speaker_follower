09a9a23f3b1359b6aa6b1f59b3803a5f3782cff6
Daniel Fried <dfried@berkeley.edu>
2018-03-29 23:07:26 +0000

    update build script and take out hard-coded paths in CMakeLists.txt

--- build.sh
+++ build.sh
@@ -8,3 +8,8 @@ cmake .. \
 -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
 -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var(LIBDIR))") \
 -DPYTHON_EXECUTABLE=$(which python)
+
+cd build
+make -j4
+
+echo "add `pwd` to your PYTHONPATH"
